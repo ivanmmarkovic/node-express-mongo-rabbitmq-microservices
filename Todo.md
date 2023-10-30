493507f937e83706756ebb4efc8c2fc781a05501
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-10-30 21:59:29 +0100

    Updated redis and rate limiter

---
+++ Todo.md
@@ -0,0 +1,7 @@
+
+
+### Create user response dto in users service
+### Add admin
+### Minio for files
+### Move jwt key in .env file
+### Add comments microservice
--- api/rateLimiter.js
+++ api/rateLimiter.js
@@ -1,6 +1,6 @@
 
-
-
+const {getClient, cacheEx} = require('./redisUtil');
+/*
 let ips = {};
 
 
@@ -30,6 +30,34 @@ async function limiter(req, res, next){
             requests: 1
         }
     }
+    next();
+}
+*/
+
+async function limiter(req, res, next){
+
+    let now = new Date().getTime();
+    let redisClient = getClient();
+    let clientCached = await redisClient.get(req.ip);
+    
+    if(clientCached){
+        let {start, requests} = JSON.parse(clientCached); 
+        if((now - start)/1000 <= 10){
+            if(requests >= 5){
+                return res.status(400).json({error: 'Too many requests'});
+            }
+            else {
+                await redisClient.set(req.ip, JSON.stringify({start, requests: requests + 1}), 'EX', cacheEx);
+            }
+        }
+        else {
+            await redisClient.set(req.ip, JSON.stringify({start: now, requests: 1}), 'EX', cacheEx);
+        }
+    }
+    else {
+        await redisClient.set(req.ip, JSON.stringify({start: now, requests: 1}), 'EX', cacheEx);
+    }
+
     next();
 }
 
